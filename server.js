9e95099a9e87f0a133c20a211ba038867bd636e9
Fox Qin <fox.qin@popturn.com>
2016-06-28 11:36:46 +0800

    install express

--- server.js
+++ server.js
@@ -2,6 +2,6 @@
 var port = process.env.port || 1337;
 //just some comments
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
 }).listen(port);
